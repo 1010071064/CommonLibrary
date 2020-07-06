37264607a4d6aeb6a61c8e83e3ec43658ca66585
1010071064 <1010071064@qq.com>
2020-07-06 22:45:01 +0800

    1、添加数据库操作类，xml操作类

--- APILibrary/DataBase.cs
+++ APILibrary/DataBase.cs
@@ -5,250 +5,249 @@ using System;
 using System.IO;
 using System.Net;
 using System.Xml;
-using System.Windows.Forms;
+//using System.Windows.Forms;
 using System.Collections;
 using System.Collections.Generic;
 using System.Web;
 using System.Text;
-using ToolClass;
 
 namespace CodeBase
 {
-    public class DataBase
-    {
-        XmlHandler xm;
-        DataSet ds = new DataSet();
+    //public class DataBase
+    //{
+    //    XmlHandler xm;
+    //    DataSet ds = new DataSet();
 
-        private string _strDBCon = "";
+    //    private string _strDBCon = "";
 
-        public DataBase()
-        {
-            UpdateDBstr();
-        }
+    //    public DataBase()
+    //    {
+    //        UpdateDBstr();
+    //    }
 
-        public void UpdateDBstr()
-        {
-            xm = new XmlHandler(CodeBase.ShareCode.WebUrl);
-            _strDBCon = xm.GetElement("SqlserverIP", "sqlurl");
-        }
+    //    public void UpdateDBstr()
+    //    {
+    //        xm = new XmlHandler(CodeBase.ShareCode.WebUrl);
+    //        _strDBCon = xm.GetElement("SqlserverIP", "sqlurl");
+    //    }
 
-        public void SetDBConInfo(string dbConInfo)
-        {
-            _strDBCon = dbConInfo;
-        }
+    //    public void SetDBConInfo(string dbConInfo)
+    //    {
+    //        _strDBCon = dbConInfo;
+    //    }
 
-        public SqlConnection SqlConn()
-        {
-            SqlConnection con = null;
-            try
-            {
-                GC.Collect();
-                con = new SqlConnection(_strDBCon);
-                con.Open();
-                return con;
-            }
-            catch (Exception e)
-            {
-                GC.Collect();
-                LogClass.Error("DataBase", "SqlConn", e);
-                return null;
-            }
-        }
+    //    public SqlConnection SqlConn()
+    //    {
+    //        SqlConnection con = null;
+    //        try
+    //        {
+    //            GC.Collect();
+    //            con = new SqlConnection(_strDBCon);
+    //            con.Open();
+    //            return con;
+    //        }
+    //        catch (Exception e)
+    //        {
+    //            GC.Collect();
+    //            LogClass.Error("DataBase", "SqlConn", e);
+    //            return null;
+    //        }
+    //    }
 
-        public SqlConnection SqlConn(string constr)
-        {
-            try
-            {
-                SqlConnection con = new SqlConnection(constr);
-                con.Open();
-                return con;
-            }
-            catch
-            {
-                return null;
-            }
-        }
+    //    public SqlConnection SqlConn(string constr)
+    //    {
+    //        try
+    //        {
+    //            SqlConnection con = new SqlConnection(constr);
+    //            con.Open();
+    //            return con;
+    //        }
+    //        catch
+    //        {
+    //            return null;
+    //        }
+    //    }
 
-        public void SqlClose(SqlConnection con)
-        {
-            if (con != null && con.State != ConnectionState.Closed)
-            {
-                con.Close();
-                con.Dispose();
-            }
-        }
-        //取出数据集 
-        public DataSet GetDataSet(string sqlstr)
-        {
-            SqlConnection con = SqlConn();
-            try
-            {
-                SqlDataAdapter fs = new SqlDataAdapter(sqlstr, con);
-                DataSet ds = new DataSet();
-                fs.Fill(ds);
-                SqlClose(con);
-                return ds;
-            }
-            catch (Exception e)
-            {
-                LogClass.Error("DataBase", "GetDataSet", e);
-                return null;
-            }
-            finally
-            {
-                if (con != null && con.State != ConnectionState.Closed)
-                {
-                    con.Close();
-                }
-                GC.Collect();
-            }
-        }
+    //    public void SqlClose(SqlConnection con)
+    //    {
+    //        if (con != null && con.State != ConnectionState.Closed)
+    //        {
+    //            con.Close();
+    //            con.Dispose();
+    //        }
+    //    }
+    //    //取出数据集 
+    //    public DataSet GetDataSet(string sqlstr)
+    //    {
+    //        SqlConnection con = SqlConn();
+    //        try
+    //        {
+    //            SqlDataAdapter fs = new SqlDataAdapter(sqlstr, con);
+    //            DataSet ds = new DataSet();
+    //            fs.Fill(ds);
+    //            SqlClose(con);
+    //            return ds;
+    //        }
+    //        catch (Exception e)
+    //        {
+    //            LogClass.Error("DataBase", "GetDataSet", e);
+    //            return null;
+    //        }
+    //        finally
+    //        {
+    //            if (con != null && con.State != ConnectionState.Closed)
+    //            {
+    //                con.Close();
+    //            }
+    //            GC.Collect();
+    //        }
+    //    }
 
-        //修改数据 
-        public bool UpDataNone(string sqlstr)
-        {
-            SqlConnection con = SqlConn();
-            try
-            {
-                SqlCommand cmd = new SqlCommand(sqlstr, con);
-                cmd.ExecuteNonQuery();
-                return true;
-            }
-            catch (Exception e)
-            {
-                LogClass.Error("DataBase", "UpDataNone", e);
-                return false;
-            }
-            finally
-            {
-                if (con != null && con.State != ConnectionState.Closed)
-                {
-                    con.Close();
-                    con.Dispose();
-                }
-                GC.Collect();
-            }
-        }
+    //    //修改数据 
+    //    public bool UpDataNone(string sqlstr)
+    //    {
+    //        SqlConnection con = SqlConn();
+    //        try
+    //        {
+    //            SqlCommand cmd = new SqlCommand(sqlstr, con);
+    //            cmd.ExecuteNonQuery();
+    //            return true;
+    //        }
+    //        catch (Exception e)
+    //        {
+    //            LogClass.Error("DataBase", "UpDataNone", e);
+    //            return false;
+    //        }
+    //        finally
+    //        {
+    //            if (con != null && con.State != ConnectionState.Closed)
+    //            {
+    //                con.Close();
+    //                con.Dispose();
+    //            }
+    //            GC.Collect();
+    //        }
+    //    }
 
-        private SqlConnection _con = null;
-        private SqlTransaction _tran = null;
-        public bool BeginUpDataNone()
-        {
-            _con = SqlConn();
-            _tran = _con.BeginTransaction(IsolationLevel.ReadCommitted);
-            return _con != null;
-        }
+    //    private SqlConnection _con = null;
+    //    private SqlTransaction _tran = null;
+    //    public bool BeginUpDataNone()
+    //    {
+    //        _con = SqlConn();
+    //        _tran = _con.BeginTransaction(IsolationLevel.ReadCommitted);
+    //        return _con != null;
+    //    }
 
-        public bool ExcuteSql(string strSql)
-        {
-            try
-            {
-                if (BeginUpDataNone())
-                {
-                    SqlCommand cmd = new SqlCommand(strSql, _con);
-                    cmd.Transaction = _tran;
-                    cmd.ExecuteNonQuery();
-                    EndUpDataNone();
-                    return true;
-                }
-            }
-            catch (Exception e)
-            {
-                LogClass.Error("DataBase", "UpDataNone", e);
-                RollBack();
-            }
-            finally
-            {
-                GC.Collect();
-            }
-            return false;
-        }
+    //    public bool ExcuteSql(string strSql)
+    //    {
+    //        try
+    //        {
+    //            if (BeginUpDataNone())
+    //            {
+    //                SqlCommand cmd = new SqlCommand(strSql, _con);
+    //                cmd.Transaction = _tran;
+    //                cmd.ExecuteNonQuery();
+    //                EndUpDataNone();
+    //                return true;
+    //            }
+    //        }
+    //        catch (Exception e)
+    //        {
+    //            LogClass.Error("DataBase", "UpDataNone", e);
+    //            RollBack();
+    //        }
+    //        finally
+    //        {
+    //            GC.Collect();
+    //        }
+    //        return false;
+    //    }
 
-        public void RollBack()
-        {
-            if (_tran != null)
-            {
-                _tran.Rollback();
-            } 
-            if (_con != null && _con.State != ConnectionState.Closed)
-            {
-                _con.Close();
-                _con.Dispose();
-            }
-        }
+    //    public void RollBack()
+    //    {
+    //        if (_tran != null)
+    //        {
+    //            _tran.Rollback();
+    //        } 
+    //        if (_con != null && _con.State != ConnectionState.Closed)
+    //        {
+    //            _con.Close();
+    //            _con.Dispose();
+    //        }
+    //    }
 
-        public void EndUpDataNone()
-        {
-            if (_tran != null)
-            {
-                _tran.Commit();
-            }
-            if (_con != null && _con.State != ConnectionState.Closed)
-            {
-                _con.Close();
-                _con.Dispose();
-            }
-        }
+    //    public void EndUpDataNone()
+    //    {
+    //        if (_tran != null)
+    //        {
+    //            _tran.Commit();
+    //        }
+    //        if (_con != null && _con.State != ConnectionState.Closed)
+    //        {
+    //            _con.Close();
+    //            _con.Dispose();
+    //        }
+    //    }
 
-        public void keypress(object sender, KeyPressEventArgs e)
-        {
-            if ((e.KeyChar < '0' && e.KeyChar != '.' || e.KeyChar > '9' && e.KeyChar != '.' || ((TextBox)(sender)).Text.IndexOf('.') >= 0 && e.KeyChar == '.') && e.KeyChar != (char)13 && e.KeyChar != (char)8)
-            {
-                e.Handled = true;
-            }
+    //    public void keypress(object sender, KeyPressEventArgs e)
+    //    {
+    //        if ((e.KeyChar < '0' && e.KeyChar != '.' || e.KeyChar > '9' && e.KeyChar != '.' || ((TextBox)(sender)).Text.IndexOf('.') >= 0 && e.KeyChar == '.') && e.KeyChar != (char)13 && e.KeyChar != (char)8)
+    //        {
+    //            e.Handled = true;
+    //        }
 
-        }
-        public void keypressint(object sender, KeyPressEventArgs e)
-        {
-            if ((e.KeyChar < '0' || e.KeyChar > '9' || ((TextBox)(sender)).Text.IndexOf('.') >= 0 && e.KeyChar == '.') && e.KeyChar != (char)13 && e.KeyChar != (char)8)
-            {
-                e.Handled = true;
-            }
+    //    }
+    //    public void keypressint(object sender, KeyPressEventArgs e)
+    //    {
+    //        if ((e.KeyChar < '0' || e.KeyChar > '9' || ((TextBox)(sender)).Text.IndexOf('.') >= 0 && e.KeyChar == '.') && e.KeyChar != (char)13 && e.KeyChar != (char)8)
+    //        {
+    //            e.Handled = true;
+    //        }
 
-        }
-        public void keypressintgrd(object sender, KeyEventArgs e)
-        {
-            if ((e.KeyValue < '0' || e.KeyValue > '9' || ((TextBox)(sender)).Text.IndexOf('.') >= 0 && e.KeyValue == '.') && e.KeyValue != (char)13 && e.KeyValue != (char)8)
-            {
-                e.Handled = true;
-            }
+    //    }
+    //    public void keypressintgrd(object sender, KeyEventArgs e)
+    //    {
+    //        if ((e.KeyValue < '0' || e.KeyValue > '9' || ((TextBox)(sender)).Text.IndexOf('.') >= 0 && e.KeyValue == '.') && e.KeyValue != (char)13 && e.KeyValue != (char)8)
+    //        {
+    //            e.Handled = true;
+    //        }
 
-        }
-        //获取省局服务器时间  add by Carrot at 2017-03-14
-        public string GetServerTime(string strSql)
-        {
-            SqlConnection con = null;
-            string result = "";
-            try
-            {
-                xm = new XmlHandler(CodeBase.ShareCode.WebUrl);
-                string sqlurl = xm.GetElement("SqlserverIP", "sqlurlTime");
-                con = new SqlConnection(sqlurl);
-                if (con == null)
-                {
-                    con.Open();
-                }
-                else if (con.State == System.Data.ConnectionState.Closed)
-                {
-                    con.Open();
-                }
-                else if (con.State == System.Data.ConnectionState.Broken)
-                {
-                    con.Close();
-                    con.Open();
-                }
-                SqlCommand cmd = new SqlCommand(strSql, con);
-                result = cmd.ExecuteScalar().ToString();
-                con.Close();
+    //    }
+    //    //获取省局服务器时间  add by Carrot at 2017-03-14
+    //    public string GetServerTime(string strSql)
+    //    {
+    //        SqlConnection con = null;
+    //        string result = "";
+    //        try
+    //        {
+    //            xm = new XmlHandler(CodeBase.ShareCode.WebUrl);
+    //            string sqlurl = xm.GetElement("SqlserverIP", "sqlurlTime");
+    //            con = new SqlConnection(sqlurl);
+    //            if (con == null)
+    //            {
+    //                con.Open();
+    //            }
+    //            else if (con.State == System.Data.ConnectionState.Closed)
+    //            {
+    //                con.Open();
+    //            }
+    //            else if (con.State == System.Data.ConnectionState.Broken)
+    //            {
+    //                con.Close();
+    //                con.Open();
+    //            }
+    //            SqlCommand cmd = new SqlCommand(strSql, con);
+    //            result = cmd.ExecuteScalar().ToString();
+    //            con.Close();
 
-            }
-            catch (Exception e)
-            {
-                con.Close();
-                return DateTime.Now.ToString();
-            }
-            return result;
-        }
+    //        }
+    //        catch (Exception e)
+    //        {
+    //            con.Close();
+    //            return DateTime.Now.ToString();
+    //        }
+    //        return result;
+    //    }
 
-    }
+    //}
 }
---
+++ APILibrary/XMLHelper/XMLHelper.cs
@@ -0,0 +1,195 @@
+using System.Data.SqlClient;
+using System.Data.Odbc;
+using System.Data;
+using System;
+using System.IO;
+using System.Net;
+using System.Xml;
+//using System.Windows.Forms;
+using System.Collections;
+using System.Collections.Generic;
+using System.Web;
+using System.Text;
+//using ToolClass;
+
+namespace APILibrary.XMLHelper
+{
+    /// <summary>自定义对配置xml文件操作</summary>
+    public class XMLHelper
+    {
+        /// <summary>XML文档</summary>
+        private System.Xml.XmlDocument _mXmlDoc = new System.Xml.XmlDocument();
+        /// <summary>XML文件路径</summary>
+        private string XmlFilePath = "";
+        /// <summary>
+        /// XML配置文档的路径
+        /// </summary>
+        public static string XMLPath
+        {
+            get
+            {
+                //return System.Windows.Forms.Application.StartupPath + "\\LocalSetting.XML";
+                return System.AppDomain.CurrentDomain.BaseDirectory + "LocalSetting.XML";
+            }
+        }
+        /// <summary>
+        /// XML文档操作构造函数
+        /// </summary>
+        /// <param name="strFilePath">XML文件路径</param>
+        public XMLHelper(string strFilePath)
+        {
+            try
+            {
+                if (!File.Exists(strFilePath))
+                {
+                    FileStream fs = File.Create(strFilePath);
+                    fs.Close();
+                    //CreateXML(strFilePath);
+                }
+                this.XmlFilePath = strFilePath;
+                _mXmlDoc.Load(XmlFilePath);
+            }
+            catch (Exception exp)
+            {
+                throw exp;//LogClass.Error("CSysXML", "CSysXML(string File)", exp);
+            }
+        }
+        /// <summary>
+        /// 创建xml声明版本信息
+        /// </summary>
+        private void CreateXML(string filePath)
+        {
+            //创建xml声明版本信息
+            XmlDocument xml = new XmlDocument();
+            xml.AppendChild(xml.CreateXmlDeclaration("1.0", "gb2312", null));
+            var el = xml.CreateElement("SystemInfo");
+            xml.AppendChild(el);
+            xml.Save(filePath);
+        }
+        
+        
+        //$--表示出错误
+        /// <summary>
+        /// 取得xml文档元素值 
+        /// </summary>
+        /// <param name="node">节点</param>
+        /// <param name="element">元素名</param>
+        /// <returns>元素值 字符型   </returns>
+        public string GetElement(string node, string element)
+        {
+            try
+            {
+                System.Xml.XmlNode mXmlNode = _mXmlDoc.SelectSingleNode("//" + node);// //越过中间节点，查询其子
+                if (mXmlNode != null)
+                {
+                    //读数据 
+                    System.Xml.XmlNode xmlNode = mXmlNode.SelectSingleNode(element);
+                    if (xmlNode != null)
+                    {
+                        return xmlNode.InnerText.ToString();
+                    }
+                }
+            }
+            catch (Exception exp)
+            {
+                throw exp;//LogClass.Error("CSysXML", "GetElement", exp);
+            }
+            return "";
+        }
+
+        /// <summary>保存元素值</summary>
+        /// <param name="node">节点名称</param>
+        /// <param name="element">元素名</param>
+        /// <param name="val">元素值</param>
+        /// <returns>True--保存成功 False--保存失败 </returns>
+        public bool SaveElement(string node, string element, string val)
+        {
+            try
+            {
+                if (!ExiteXmlDeclaration())
+                {
+                    XmlNode dec = _mXmlDoc.CreateXmlDeclaration("1.0", "gb2312", "");
+                    _mXmlDoc.AppendChild(dec);
+                }
+                System.Xml.XmlNode xmlRoot = _mXmlDoc.SelectSingleNode("//" + "SystemIP");
+                if (xmlRoot == null)
+                {
+                    xmlRoot = _mXmlDoc.CreateElement("", "SystemIP", "");
+                    _mXmlDoc.AppendChild(xmlRoot);
+                }
+                System.Xml.XmlNode mXmlNode = xmlRoot.SelectSingleNode("//" + node);
+                if (mXmlNode == null)
+                {
+                    mXmlNode = _mXmlDoc.CreateElement("", node, "");
+                    xmlRoot.AppendChild(mXmlNode);
+                }
+
+                System.Xml.XmlNode xmlNodeNew = mXmlNode.SelectSingleNode(element);
+
+                if (xmlNodeNew == null)
+                {
+                    xmlNodeNew = _mXmlDoc.CreateElement("", element, "");
+                    mXmlNode.AppendChild(xmlNodeNew);
+                }
+                xmlNodeNew.InnerText = val;
+                _mXmlDoc.Save(this.XmlFilePath);
+
+                return true;
+            }
+            catch (Exception exp)
+            {
+                //LogClass.Error("SaveElement", exp);
+                throw exp;
+            }
+        }
+
+        /// <summary>判断文档是否添加了声明</summary>
+        /// <returns>treu：添加了，false:未添加</returns>
+        private bool ExiteXmlDeclaration()
+        {
+            if (_mXmlDoc != null)
+            {
+                if (_mXmlDoc.ChildNodes.Count > 0)
+                {
+                    System.Xml.XmlNode xmlRoot = _mXmlDoc.ChildNodes[0];
+                    if (xmlRoot.NodeType == XmlNodeType.XmlDeclaration)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+
+        /// <summary>
+        /// 添加节点，或属性
+        /// </summary>
+        /// <param name="xml">xml文档</param>
+        /// <param name="nodeName">节点名</param>
+        /// <returns></returns>
+        public XmlNode AddNode(XmlDocument xml, string nodeName)
+        {
+            XmlNode newNode = xml.CreateNode(XmlNodeType.Element, nodeName,null);
+            //添加节点属性如：
+            //newNode.Attributes.Append(AddAttribute(xml, "attrName", "attrValue"));
+            return newNode;
+        }
+
+
+        /// <summary>
+        /// 添加一个属性
+        /// </summary>
+        /// <param name="xml">xml文档</param>
+        /// <param name="attributeName">属性名</param>
+        /// <param name="attributeValue">属性值</param>
+        /// <returns>返回属性</returns>
+        public XmlAttribute AddAttribute(XmlDocument xml, string attributeName, string attributeValue)
+        {
+            XmlAttribute xmlAttr = xml.CreateAttribute(attributeName);
+            xmlAttr.Value = attributeValue;
+            return xmlAttr;
+        }
+    }
+
+}
